bf1a1636ef63ec7c6d389692ec4368243655cf49
eliezer <eliezer.moraes@outlook.com>
2023-09-05 20:42:58 -0300

    finalizando aula 02. Variáveis e operadores

--- aula 02 - variaveis/script.js
+++ aula 02 - variaveis/script.js
@@ -2,13 +2,13 @@
 
 // boolean
 var vOuF = false;
-console.log(typeof(vOuF))
+console.log(typeof(vOuF));
 
-//number
+// number
 var numeroQualquer = 1;
 console.log(typeof(numeroQualquer));
 
-//string
+// string
 var nome = 'Eliezer'
 console.log(typeof(nome));
 
@@ -23,7 +23,7 @@ tiver um valor inicial será tratada como null;
 const – escopo local de bloco, somente leitura, o valor inicial é
 obrigatório e não pode ser alterado.*/
 
-//como declarar
+// como declarar
 var variavel = 'Eliezer';
 variavel = 'Nádia';
 console.log(variavel);
@@ -55,3 +55,128 @@ function escopoLocal() {
 }
 
 escopoLocal();
+
+/*
+Atribuição
+
+O sinal de igualdade “=“ em JavaScript, significa atribuição.
+
+Ex.:
+como declarar: var nome = “meu nome”;
+como ler: variável nome recebe o valor meunome;
+*/
+
+// atribuição
+var atribuicao = 'Eliezer';
+
+
+/*
+Para fazermos uma comparação de valores em JavaScript usamos
+“==“.
+
+Ex.:
+como declarar: "0" == 0; ;
+como ler: “0” tem o valor igual a 0?
+
+// nesse caso retorna true
+*/
+// comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+/*
+Para fazermos uma comparação de valores e tipos em JavaScript
+usamos “===“.
+
+Ex.:
+como declarar: "0" === 0; ;
+como ler: “0” tem o valor e o tipo idêntico a 0?
+
+// nesse caso retorna false
+*/
+
+// comparação idêntica
+var comparacaoIdentica = '0' === 0
+console.log(comparacaoIdentica)
+
+/*
+Operadores aritméticos
+São tipos de operadores matemáticos com valor numérico:
+
+• + adição;
+• - subtração;
+• * multiplicação;
+• / divisão real;
+• % divisão inteira;
+• ** potenciação;
+*/
+
+// adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subtracao
+var subtracao = 1 - 1;
+console.log(subtracao);
+
+// multiplicao
+var multiplicao = 2 * 3;
+console.log(multiplicao);
+
+// multiplicao
+var divisao = 6 / 2;
+console.log(divisao);
+
+// divisao inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao);
+
+/*
+Operadores relacionais
+
+São tipos de operadores que consultam a relação entre valores:
+
+• > maior que;
+• < menor que;
+• >= maior ou igual a;
+• <= menor ou igual a;
+*/
+
+// maior que
+var maiorQue = 5 > 3;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 6 < 10;
+console.log(menorQue);
+
+// maior ou igual que
+var maiorOuIgualQue = 5 >= 6;
+console.log(maiorOuIgualQue);
+
+// menor ou igual que
+var menorOuIgualQue = 6 <= 1;
+console.log(menorOuIgualQue);
+
+/*
+Operadores lógicos
+
+São tipos de operadores que consultam valores lógicos:
+
+• && - “e” – considera que todos os valores sejam true;
+• || - “ou” – considera que qualquer valor seja true;
+• ! - “não” – inverte o valor de true para false ou vice-versa;
+*/
+
+var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
