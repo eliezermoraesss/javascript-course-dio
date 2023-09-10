069bd7ea78353af376d673312aa1a3ad594a5561
eliezer <eliezer.moraes@outlook.com>
2023-09-10 14:33:21 -0300

    aula 05 - funcoes

---
+++ aula 05 - funcoes/script.js
@@ -0,0 +1,11 @@
+function funcao() {
+    console.log('Tudo certo meu caro!');
+}
+
+funcao();
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+
+mensagem('Tudo bem', 'meu querio')
